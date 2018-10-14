b676c0ee73a9262b4205b034381d1e8541994b7f
iamwebwiz <iamwebwiz@gmail.com>
2018-10-14 10:06:49 +0100

    update github controller

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client as GuzzleClient;
 use Illuminate\Http\Request;
 
 class GithubController extends Controller
 {
-    //
+    protected $guzzleclient;
+
+    public function __construct()
+    {
+        $this->guzzleclient = new GuzzleClient();
+    }
 }
