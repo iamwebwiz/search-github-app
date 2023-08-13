f601a41d6219eec6ee961fd70b6bd97514d6415b
Ezekiel <iamwebwiz@gmail.com>
2023-08-13 12:27:44 +0100

    setup daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ export default function App() {
   return (
     <div>
       <h1 className="font-bold text-3xl">Hello World</h1>
+      <button className="btn btn-primary">Button from DaisyUI!</button>
     </div>
   );
 }
