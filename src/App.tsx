8186eadd19020cabe2c3675d850f43f2eb7a51ab
Ezekiel <iamwebwiz@gmail.com>
2023-08-14 02:32:25 +0100

    refactors - extract components & types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
+import ErrorMessage from "./components/ErrorMessage";
+import UserCard from "./components/UserCard";
+import { ISearchResultState, User } from "./types";
 
 export default function App() {
   const [username, setUsername] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
-  const [searchResult, setSearchResult] = useState<{ query: string; runCount: number; user: any }>({
+  const [searchResult, setSearchResult] = useState<ISearchResultState>({
     query: "",
     runCount: 0,
     user: {},
@@ -13,10 +16,8 @@ export default function App() {
     event.preventDefault();
     setLoading(true);
 
-    console.log("form submitted with username as", username);
-
     const response = await fetch(`https://api.github.com/users/${username}`);
-    const data: any = await response.json();
+    const data: User = await response.json();
 
     setSearchResult((prev) => {
       return {
@@ -28,10 +29,6 @@ export default function App() {
     });
     setLoading(false);
   }
-
-  useEffect(() => {
-    console.log("search result", searchResult);
-  }, [searchResult]);
 
   return (
     <div className="pt-10 px-5 max-w-screen sm:px-14">
@@ -61,42 +58,9 @@ export default function App() {
                 <h1 className="text-center text-3xl">Search Result</h1>
               </div>
               {searchResult.user.message ? (
-                <div className="alert alert-error flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="stroke-current shrink-0 h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                  <span>
-                    <span className="font-bold">GitHub user "{searchResult.query}" not found!</span>
-                  </span>
-                </div>
+                <ErrorMessage query={searchResult.query} />
               ) : (
-                <div>
-                  <div className="card card-compact w-full sm:max-w-xs bg-base-100 shadow-xl mb-3">
-                    <figure>
-                      <img src={searchResult.user.avatar_url} alt="Shoes" />
-                    </figure>
-                    <div className="card-body">
-                      <h2 className="card-title">{searchResult.user.name}</h2>
-                      <h3 className="text-lg">{searchResult.user.login}</h3>
-                      <p>{searchResult.user.bio}</p>
-                      <div className="card-actions justify-start mt-3">
-                        <a href={searchResult.user.html_url} className="btn w-full btn-neutral" target="_blank">
-                          Go to profile
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <UserCard user={searchResult.user} />
               )}
             </>
           )}
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,21 @@
+import { User } from "../types";
+
+export default function UserCard(props: { user: User }) {
+  return (
+    <div className="card card-compact w-full sm:max-w-xs bg-base-100 shadow-xl mb-3">
+      <figure>
+        <img src={props.user.avatar_url} alt={`${props.user.login} avatar`} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{props.user.name}</h2>
+        <h3 className="text-lg">{props.user.login}</h3>
+        <p>{props.user.bio}</p>
+        <div className="card-actions justify-start mt-3">
+          <a href={props.user.html_url} className="btn w-full btn-neutral" target="_blank">
+            Go to profile
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
